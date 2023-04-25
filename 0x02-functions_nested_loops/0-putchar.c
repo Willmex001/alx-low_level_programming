8010ce64e334978a931a5ab9bdf121ac5c7a69af
Willmex001 <janealan03@gmail.com>
2023-04-25 07:58:18 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
  * main - Entry point
+ * Description: 'printv putchar'
  * Return: Always 0
  */
-
 int main(void)
 {
 	_putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        return (0);
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
 }
