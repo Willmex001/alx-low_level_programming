87be8deaa86c03b37d3bb05ccb0f5021c98b8bdb
Willmex001 <janealan03@gmail.com>
2023-05-09 02:55:49 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, n, value, check;
+
+	value = 0;
+
+	for (i = 0; s[i] != '\n'; i++)
+	{
+		check = 0;
+
+		for (n = 0; accept[n] != '\n'; n++)
+		{
+			if (accept[n] == s[i])
+			{
+				value++;
+				check = 1;
+			}
+		}
+	}
+}
