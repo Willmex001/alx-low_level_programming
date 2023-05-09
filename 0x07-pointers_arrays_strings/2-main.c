6509e339beee8521729476be7237bb88fb8dd188
Willmex001 <janealan03@gmail.com>
2023-05-08 19:52:10 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -11,7 +12,7 @@ int main(void)
 	char *s = "hello";
 	char *f;
 
-	f = _strchr(s, 'l');
+	f = strchr(s, 'l');
 	if (f != NULL)
 	{
 		printf("%s\n", f);
